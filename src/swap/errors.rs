ae1f0645da9ea7a6e27c192772f9ca5574948fa9
havis <h4vismat@mooze.app>
2025-09-16 05:20:31 -0300

    Update base errors and models.

--- src/swap/errors.rs
+++ src/swap/errors.rs
@@ -1,9 +1,15 @@
 use thiserror::Error;
 
-use crate::{models::Asset, swap::api};
+use crate::{errors::WalletError, models::Asset, swap::api};
 
 #[derive(Debug, Error)]
 pub enum SwapError {
+    #[error("Sideswap error: {0}")]
+    SideswapError(#[from] api::sideswap::SideswapError),
+    #[error("Wallet error: {0}")]
+    WalletError(#[from] WalletError),
+    #[error("Invalid quote")]
+    InvalidQuote,
     #[error("Invalid market.")]
     InvalidMarket,
     #[error("Quote expired.")]
@@ -16,8 +22,12 @@ pub enum SwapError {
     ContextError(String),
     #[error("Insufficient amount.")]
     InsufficientFunds,
-    #[error("Sideswap error: {0}")]
-    SideswapError(#[from] api::sideswap::SideswapError),
     #[error("Invalid asset")]
-    InvalidAsset
+    InvalidAsset,
+    #[error("Amount below min peg-out limit.")]
+    AmountBelowMinPegOut,
+    #[error("Amount below minimum: {0}")]
+    AmountBelowMinimum(u64),
+    #[error("Amount above maximum: {0}")]
+    AmountAboveMax(u64),
 }
